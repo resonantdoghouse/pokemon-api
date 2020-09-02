0f1d512ea2d936e34d417383f34cfa0eb3432f36
Jim <jim@advancebass.com>
2020-09-02 07:49:24 -0700

    Updates

---
+++ precache-manifest.03d88d6fc8a78943fd6cdd1139e11f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cb520fd0ac01c98065cb6ad4346920",
+    "url": "/pokemon-api/index.html"
+  },
+  {
+    "revision": "600b5292050b02871be8",
+    "url": "/pokemon-api/static/css/main.1025e7fc.chunk.css"
+  },
+  {
+    "revision": "77f1a566d72046122a9d",
+    "url": "/pokemon-api/static/js/2.d1844ad3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemon-api/static/js/2.d1844ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600b5292050b02871be8",
+    "url": "/pokemon-api/static/js/main.43b0526a.chunk.js"
+  },
+  {
+    "revision": "0eb6e96b81eae8ef3f31",
+    "url": "/pokemon-api/static/js/runtime-main.cdf0dd49.js"
+  }
+]);
